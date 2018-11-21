30025258e5bd429e91808bf423a5d746b6bc0006
Hanna Landerus <you@example.com>
2018-11-21 21:16:19 +0100

    Updates

---
+++ precache-manifest.c1815a56b61e30d314042542d0e830cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/woofout/static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "5b5c1810d89f1f366b80",
+    "url": "/woofout/static/js/runtime~main.5b5c1810.js"
+  },
+  {
+    "revision": "0aa76ef2d9836b55050d",
+    "url": "/woofout/static/js/main.0aa76ef2.chunk.js"
+  },
+  {
+    "revision": "1b49c78145cb06262acb",
+    "url": "/woofout/static/js/1.1b49c781.chunk.js"
+  },
+  {
+    "revision": "0aa76ef2d9836b55050d",
+    "url": "/woofout/static/css/main.169a296d.chunk.css"
+  },
+  {
+    "revision": "541d7992a80d6b192d6495b9a2fae122",
+    "url": "/woofout/index.html"
+  }
+];
